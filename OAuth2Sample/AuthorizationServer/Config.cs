895a81d76372d67b60b2cca9f6cfa4b3bc869532
서울 집(메인) <1dong8@hanmail.net>
2019-11-08 04:04:05 +0900

    인증서버 완료

---
+++ OAuth2Sample/AuthorizationServer/Config.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using IdentityServer4.Models;
+
+namespace AuthorizationServer
+{
+    public class Config
+    {
+        // scopes define the API resources in your system
+        //API의 인증 범위를 정의한다.
+        public static IEnumerable<ApiResource> GetApiResources()
+        {
+            return new List<ApiResource>
+            {
+                new ApiResource("scope.readaccess", "Example API"),
+                new ApiResource("scope.fullaccess", "Example API"),
+                new ApiResource("YouCanActuallyDefineTheScopesAsYouLike", "Example API")
+            };
+        }
+
+        // client wants to access resources (aka scopes)
+        //클라이언트 접근 범위를 설정한다.
+        public static IEnumerable<Client> GetClients()
+        {
+            return new List<Client>
+            {
+                new Client
+                {
+                    ClientId = "ClientIdThatCanOnlyRead"
+                    , AllowedGrantTypes = GrantTypes.ClientCredentials
+
+                    , ClientSecrets = { new Secret("secret1".Sha256())}
+                    , AllowedScopes = { "scope.readaccess" }
+                }
+                , new Client
+                {
+                    ClientId = "ClientIdWithFullAccess"
+                    , AllowedGrantTypes = GrantTypes.ClientCredentials
+
+                    , ClientSecrets = { new Secret("secret2".Sha256()) }
+                    , AllowedScopes = { "scope.fullaccess" }
+                }
+            };
+        }
+
+    }
+}
---
+++ OAuth2Sample/AuthorizationServer/Startup.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AuthorizationServer
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //AddInMemoryApiResources   : OAuth2 미들웨어(IdentityServer) 설정
+            //AddInMemoryClients        : OAuth2 미들웨어(IdentityServer) 설정
+            services.AddIdentityServer()
+                    .AddInMemoryApiResources(Config.GetApiResources())
+                    .AddInMemoryClients(Config.GetClients());
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            //OAuth2 미들웨어(IdentityServer) 설정
+            app.UseIdentityServer();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            //app.Run(async (context) =>
+            //{
+            //    await context.Response.WriteAsync("Hello World!");
+            //});
+
+
+            //웹사이트 기본파일 읽기 설정
+            app.UseDefaultFiles();
+            //wwwroot 파일읽기
+            app.UseStaticFiles();
+            //WebAPI 설정
+            app.UseMvc();
+        }
+
+
+    }
+}
